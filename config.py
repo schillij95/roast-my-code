62c347b49005d8825337b85819bf011b3ad64f79
Julian Schilliger <julian@scrollprize.org>
2025-06-15 08:48:29 -0700

    Roasting GitHub profiles since 1995

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ PROMPT_GITHUB_PROFILE_TEMPLATE = """
 To make it easier for you to roast the Profile, we already parsed some information for you and run a sumarizing critique on the files of the pinned repositories. You can also use the stars, followers, and other information to roast the profile.
 Above is the GitHub profile and the result of the preliminary summary. 
 Deliver your roast in the following style: {roast_style}
-Your answer should not be a novel. Dont go into detail on too many files, pick the worst offenders and focus on them if any specific files at all.
+Your answer should not be a novel. Dont go into detail on too many files, pick the worst offenders and focus on them if any specific files at all. Disregard None fields.
 
 Use this information to completely destroy the profile.
 """ + SYSTEM_PROMPT.format(code_type="GitHub profiles") + """
